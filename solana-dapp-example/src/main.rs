d16bbbf963b277611d0b5cd0ae71214a1bbff650
Russell Wong <russellwmy@gmail.com>
2022-03-10 09:36:36 +0800

    Revert "feat: yew use_state hook"

--- solana-dapp-example/src/main.rs
+++ solana-dapp-example/src/main.rs
@@ -1,4 +1,3 @@
-mod button;
 mod phantom;
 mod phantom_connect;
 
@@ -10,6 +9,7 @@ fn app() -> Html {
     html! {
         <div class="w-screen h-screen flex items-center justify-center">
             <div class="flex flex-col items-center justify-center">
+                <h1  class="p-10 text-xl font-bold">{ "Connect to Phantom Wallet" }</h1>
                 <PhantomConnect/>
             </div>
         </div>
